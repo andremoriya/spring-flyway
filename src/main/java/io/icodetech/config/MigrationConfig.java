fad80afcc502498255c3aca58c22432f73f26f74
André Heidi Moriya <aheidi.moriya@gmail.com>
2018-09-01 22:58:40 -0300

    Change flyway configuration

--- src/main/java/io/icodetech/config/MigrationConfig.java
+++ src/main/java/io/icodetech/config/MigrationConfig.java
@@ -2,9 +2,21 @@ package io.icodetech.config;
 
 import org.flywaydb.core.Flyway;
 
+import javax.sql.DataSource;
+
 public class MigrationConfig extends Flyway {
 
+    private MigrationConfig(DataSource dataSource) {
+        this.setDataSource(dataSource);
+    }
+
+    public static MigrationConfig of(DataSource dataSource) {
+        return new MigrationConfig(dataSource);
+    }
+
     public void repairAndMigrate() {
+        this.setBaselineOnMigrate(true);
+        this.setLocations("classpath:db/migration");
         this.repair();
         this.migrate();
     }
